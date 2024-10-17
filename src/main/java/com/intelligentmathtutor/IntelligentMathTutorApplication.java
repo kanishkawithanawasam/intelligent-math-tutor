f716481e0705cd95d0bbd03aec4190a54ec6dd4b
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2024-10-17 22:38:35 +0100

    init: Project setup

---
+++ src/main/java/com/intelligentmathtutor/IntelligentMathTutorApplication.java
@@ -0,0 +1,33 @@
+package com.intelligentmathtutor;
+
+import com.intelligentmathtutor.controllers.LogicController;
+import com.intelligentmathtutor.models.questions.numbers.NumbersL1Q1;
+import com.intelligentmathtutor.models.questions.numbers.NumbersL1Q2;
+
+//@SpringBootApplication
+public class IntelligentMathTutorApplication {
+
+    public static void main(String[] args) {
+
+        //SpringApplication.run(IntelligentMathTutorApplication.class, args);
+        LogicController controller = new LogicController();
+
+        for (int i = 0; i < 3; i++) {
+            String q = (new NumbersL1Q1()).toString();
+            System.out.println(q + " = "+controller.eval(q).toNearestInt());
+            System.out.println(q + " = "+controller.eval(q).to2decimals());
+            System.out.println();
+
+        }
+
+        for (int i = 0; i < 5; i++) {
+            String q = (new NumbersL1Q2()).toString();
+            System.out.println(q + " = "+controller.eval(q).toNearestInt());
+            System.out.println(q + " = "+controller.eval(q).to2decimals());
+            System.out.println();
+        }
+
+    }
+
+
+}
---
+++ src/test/java/com/intelligentmathtutor/IntelligentMathTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelligentmathtutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentMathTutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
