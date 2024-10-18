2cf3d7cd906d2a70e778c2069f1b12e17224046f
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2024-10-18 14:50:53 +0100

    Addition: NumbersL1Q4 expressions with brackets

---
+++ src/main/java/com/intelligentmathtutor/models/questions/numbers/NumbersL1Q4.java
@@ -0,0 +1,51 @@
+package com.intelligentmathtutor.models.questions.numbers;
+
+import com.intelligentmathtutor.models.questions.Question;
+
+/**
+ * Questions with brackets.
+ */
+public class NumbersL1Q4 extends Question {
+
+    public NumbersL1Q4() {
+        super(1, 25,"Solve the expression" +
+                "and provide the answer to 2 decimal places."); // Set base settings
+
+        int preBracketTerms = (int) this.getRandomNumber(1,3);
+        int postBracketTerms = (int) this.getRandomNumber(1,3);
+        int inBracketTerms = (int) this.getRandomNumber(1,4);
+        StringBuilder question= new StringBuilder();
+        for (int i = 0; i < preBracketTerms; i++) {
+            question.append(this.getRandomNumber(1, 10));
+            question.append(this.getOperator());
+
+        }
+
+        // Add bracket term
+        question.append("(");
+        for (int i = 0; i < inBracketTerms; i++) {
+            question.append(this.getRandomNumber(1, 10));
+            if(i!=inBracketTerms-1){
+                question.append(this.getOperator());
+            }
+        }
+        question.append(")");
+
+        // Add an operator after brackets closed
+        question.append(this.getOperator());
+
+        // Post bracket terms
+        for (int i = 0; i < postBracketTerms; i++) {
+            question.append(this.getRandomNumber(1, 10));
+            if(i!=postBracketTerms-1){
+                question.append(this.getOperator());
+            }
+        }
+
+        setQuestion(question.toString());
+    }
+
+
+
+
+}
