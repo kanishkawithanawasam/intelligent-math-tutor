0a7e0d4c306ef290ce71c28a3220e2455101c2bd
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2024-10-18 02:15:49 +0100

    Refactor: String builder removed

--- src/main/java/com/intelligentmathtutor/models/questions/numbers/NumbersL1Q1.java
+++ src/main/java/com/intelligentmathtutor/models/questions/numbers/NumbersL1Q1.java
@@ -2,16 +2,22 @@ package com.intelligentmathtutor.models.questions.numbers;
 
 import com.intelligentmathtutor.models.questions.Question;
 
+/**
+ * Questions with 2 terms and a single operator
+ */
 public class NumbersL1Q1 extends Question {
 
 
     public NumbersL1Q1() {
         super(1, 15); // Set base settings
-        StringBuilder builder = new StringBuilder();
-        builder.append(this.getRandomNumber(1,10));
-        builder.append(this.getOperator());
-        builder.append(this.getRandomNumber(1,10));
-        this.setQuestion(builder.toString());
+
+        // Generate question
+        String question = String.valueOf(this.getRandomNumber(1, 10)) +
+                this.getOperator() +
+                this.getRandomNumber(1, 10);
+
+        // Set question
+        this.setQuestion(question);
     }
 
 
